49203c1bd215665b9223a88bebe369ee10e1f19c
kass-andra <ul7610@pm.me>
2024-05-20 22:21:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,19 +9,27 @@ dates = ["24 мая (пт)", "25 мая (сб)", "26 мая (вс)", "27 мая 
 times = ["10-12", "12-14", "14-16", "16-18", "18-20", "20-22", "22-24"]
 
 columns = pd.MultiIndex.from_product([dates, times])
+columns_sum = 
 
+# Создаем пустой DataFrame с мультииндексом колонок для данных
 df = pd.DataFrame(columns=columns)
 
-# Функция для отображения чекбоксов и обновления таблицы
-# def update_schedule(df):
+# Создаем пустой DataFrame с мультииндексом колонок для сумм
+sums_df = pd.DataFrame(0, index=['Сумма'], columns=columns)
+
+# # Функция для отображения чекбоксов и обновления таблицы
+# def update_schedule(df, sums_df):
 #     st.title("Расписание собраний")
 #     st.write("Выберите удобные временные интервалы:")
 
 #     with st.form(key='schedule_form'):
 #         selected_times = {}
-#         for col in df.columns:
-#             day, time = col
-#             selected_times[col] = st.checkbox(f"{day} {time}")
+#         for date in dates:
+#              st.write(f"{date}")
+#              cols = st.columns(len(times))
+#              for i, time in enumerate(times):
+#                  col_key = (date, time)
+#                  selected_times[col_key] = cols[i].checkbox(time, key=f"{date}-{time}")
         
 #         submitted = st.form_submit_button("Отправить")
 
@@ -30,18 +38,16 @@ df = pd.DataFrame(columns=columns)
 #         for col in df.columns:
 #             new_row.append(1 if selected_times[col] else 0)
         
-#         # Добавляем новую строку в DataFrame
+#         # Добавляем новую строку в DataFrame данных
 #         df.loc[len(df)] = new_row
+
+#         # Обновляем суммы в DataFrame сумм
+#         sums_df.loc['Сумма'] = df.sum(axis=0)
         
 #         st.success("Ваши данные были успешно обновлены!")
         
-#     return df
+#     return df, sums_df
 
-# Создаем пустой DataFrame с мультииндексом колонок для данных
-df = pd.DataFrame(columns=columns)
-
-# Создаем пустой DataFrame с мультииндексом колонок для сумм
-sums_df = pd.DataFrame(0, index=['Сумма'], columns=columns)
 
 # Функция для отображения чекбоксов и обновления таблицы
 def update_schedule(df, sums_df):
@@ -50,13 +56,39 @@ def update_schedule(df, sums_df):
 
     with st.form(key='schedule_form'):
         selected_times = {}
+
+        # Отображение чекбоксов в виде таблицы
+        st.write("<style>div.row-widget.stRadio>div{flex-direction:row;}</style>", unsafe_allow_html=True)
+        st.write("<style>.main_container{display: flex; flex-direction: row;}</style>", unsafe_allow_html=True)
+        st.write("<style>.widgetrow{display: flex; flex-direction: row;}</style>", unsafe_allow_html=True)
+        st.write("<style>.widgetcol{display: flex; flex-direction: column;}</style>", unsafe_allow_html=True)
+
+        st.write("<div class='main_container'>", unsafe_allow_html=True)
+        st.write("<div class='widgetcol'>", unsafe_allow_html=True)
+
+        # Заголовки столбцов
+        st.write("<div class='widgetrow'>", unsafe_allow_html=True)
+        st.write("<div class='widgetcol'></div>", unsafe_allow_html=True)
+        for time in times:
+            st.write(f"<div class='widgetcol'>{time}</div>", unsafe_allow_html=True)
+        st.write("</div>", unsafe_allow_html=True)
+
         for date in dates:
-             st.write(f"#### {date}")
-             cols = st.columns(len(times))
-             for i, time in enumerate(times):
-                 col_key = (date, time)
-                 selected_times[col_key] = cols[i].checkbox(time, key=f"{date}-{time}")
-        
+            # Дата в качестве заголовка строки
+            st.write("<div class='widgetrow'>", unsafe_allow_html=True)
+            st.write(f"<div class='widgetcol'>{date}</div>", unsafe_allow_html=True)
+            
+            # Чекбоксы для временных интервалов
+            for time in times:
+                col_key = (date, time)
+                selected_times[col_key] = st.checkbox('', key=f'{date}-{time}')
+                st.write(f"<div class='widgetcol'>{selected_times[col_key]}</div>", unsafe_allow_html=True)
+            
+            st.write("</div>", unsafe_allow_html=True)
+
+        st.write("</div>", unsafe_allow_html=True)
+        st.write("</div>", unsafe_allow_html=True)
+
         submitted = st.form_submit_button("Отправить")
 
     if submitted:
@@ -75,9 +107,9 @@ def update_schedule(df, sums_df):
     return df, sums_df
 
 
-# Отображение текущего состояния таблицы данных
-st.write("Текущее состояние таблицы:")
-st.write(df)
+# # Отображение текущего состояния таблицы данных
+# st.write("Текущее состояние таблицы:")
+# st.write(df)
 
 # Обновление расписания
 df, sums_df = update_schedule(df, sums_df)
